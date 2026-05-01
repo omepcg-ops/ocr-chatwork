160fb189d27089c7bf8fed9512ebadff3f547ea8
omepcg-ops <omepcg@gmail.com>
2026-05-01 23:40:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
+app.use(express.static("public"));
 
 const upload = multer({ storage: multer.memoryStorage() });
 
